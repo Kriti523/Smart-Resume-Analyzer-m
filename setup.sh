c2d614cf7e3885f19373e8eb17312874cb5e15bb
Kriti523 <kritijain9414@gmail.com>
2025-04-20 17:25:12 +0530

    Fixed pdfminer imports and requirements

--- setup.sh
+++ setup.sh
@@ -3,5 +3,4 @@ python -m pip install --upgrade pip
 python -m nltk.downloader stopwords
 python -m nltk.downloader punkt
 python -m nltk.downloader averaged_perceptron_tagger
-python -m nltk.downloader universal_tagset
 python -m spacy download en_core_web_sm
